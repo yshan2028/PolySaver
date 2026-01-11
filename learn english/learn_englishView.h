8ed8340c9f99bb822e38458b905dd62393ff6efd
yshan2028 <yshan2028@gmail.com>
2026-01-12 00:12:57 +0800

    Initial Commit

---
+++ learn english/learn_englishView.h
@@ -0,0 +1,12 @@
+//
+//  learn_englishView.h
+//  learn english
+//
+//  Created by 单应青 on 1/12/26.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface learn_englishView : ScreenSaverView
+
+@end
